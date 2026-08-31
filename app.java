4a11ee05f88fe6921ae86ceca17055ba499dce18
Suubbu-ai <chenisubbu183@gmail.com>
2026-08-31 15:07:17 +0530

    Fix main method and correct arithmetic output

--- app.java
+++ app.java
@@ -1,9 +1,9 @@
 public class app {
-	public static void main (string[]args)
-	{
-		int a=10;
-		int b=5;
-		System.out.println("addition is:"+(a+b));
-		 System.out.println("difference is:"+(a+b));
-	}
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition is: " + (a + b));
+        System.out.println("Difference is: " + (a - b));
+    }
 }
