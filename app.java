3846d46c1fd3fe6546f0ba4505e8a7ddfd89e404
Suubbu-ai <chenisubbu183@gmail.com>
2026-08-31 15:06:14 +0530

    Fix formatting and case issues in app.java

--- app.java
+++ app.java
@@ -1,6 +1,9 @@
 public class app {
-	public static void main (string[]args){
+	public static void main (string[]args)
+	{
 		int a=10;
 		int b=5;
-		system.out.println("addition is:"+(a+b));
-		 system.out.println("difference is:"+(a+b));
+		System.out.println("addition is:"+(a+b));
+		 System.out.println("difference is:"+(a+b));
+	}
+}
